8522657bdb28ac294f4399399a9976ce15a42701
kc emmanuel <fidekg123@gmail.com>
2021-04-18 04:39:09 +0100

    'python_re_for_email'

--- main.py
+++ main.py
@@ -1,40 +1,10 @@
-name = ['name', 'title', 'agents']
+import re 
 
-l = []
-for item in name:
-    l.append(item)
-print(l)
+text = 'random string Myname123@website.com some more random text YourName13435@company.com'
 
-import turtle
-kc_tutrtle = turtle.Turtle(shape='classic')
-kc_tutrtle.color('green', 'blue')
-for i in range(0,4):
-    kc_tutrtle.forward(100)
-    kc_tutrtle.right(90)
-    kc_tutrtle.forward(100)
-    kc_tutrtle.right(90)
-    kc_tutrtle.forward(100)
-    kc_tutrtle.right(90)
-    kc_tutrtle.forward(100)
-    kc_tutrtle.forward(100)
-    kc_tutrtle.circle(60)
-   
-   
-print([person for person in name])
+pattern = re.compile('[a-yA-Y1-9]+@[a-yA-Y1-9]+\.[a-yA-Y]+')
 
-num = [1, 2, 3, 4]
+result = pattern.findall(text)
+print(result)
 
-print([2 * num1 for num1 in num])
-
-movies_and_rating = {
-    'Intersteller':6.59,
-    'Dark Night': 6,
-    'Avangers':9.5,
-    'justiclegue':6.8,
-    'Spiderman': 8.5
-}
-l = []
-
-
-print( [movie for movie in movies_and_rating if movies_and_rating[movie] > 6] )
-
+    
